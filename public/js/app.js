40f1caa36926b5b4cc3935b183f62e40144b8e59
Guru Samrudha <gurusamrudha@gurus-mbp.home>
2021-05-21 10:07:01 +0100

    cleaning app.js client js

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,7 @@
-console.log('Logging from the client side JS');
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
-
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
